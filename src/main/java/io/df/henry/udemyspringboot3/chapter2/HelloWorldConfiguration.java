067c22c451dad4a2864ea386bb8fa2312aeabaa1
henry <dogfootmaster@gmail.com>
2024-02-06 01:11:56 +0900

    feat: bean test code 추가.

--- src/main/java/io/df/henry/udemyspringboot3/chapter2/HelloWorldConfiguration.java
+++ src/main/java/io/df/henry/udemyspringboot3/chapter2/HelloWorldConfiguration.java
@@ -1,7 +1,9 @@
 package io.df.henry.udemyspringboot3.chapter2;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 record Person(String name, int age, Address address){}
 record Address(String firstLine, String city){}
@@ -20,27 +22,36 @@ public class HelloWorldConfiguration {
   }
 
   @Bean
+  @Primary
   public Person person() {
     return new Person("George", 38, new Address("Baker street", "London"));
   }
 
   @Bean
   public Person person2MethodCall() {
-    return new Person(name(), age(), address());
+    return new Person(name(), age(), address2());
   }
 
   @Bean
-  public Person person3Parameters(String name, int age, Address address3) {
+  public Person person3Parameters(String name, int age, @Qualifier("address3-qualifier") Address address3) {
+    // address3이름으로 빈 매칭을 할줄 알았는데 어쨌든 타입으로 매칭하여 qualifier를 해줌.
     return new Person(name, age, address3);
   }
 
+  @Bean
+  public Person person4Parameters(String name, int age, Address address) {
+    return new Person(name, age, address);
+  }
+
   @Bean(name = "address2")
-  public Address address() {
+  @Primary
+  public Address address2() {
     return new Address("Baker street", "London");
   }
 
   @Bean(name = "address3")
-  public Address address2() {
+  @Qualifier("address3-qualifier")
+  public Address address3() {
     return new Address("Donggyo-ro 1-gil", "Seoul");
   }
 
--- src/main/java/io/df/henry/udemyspringboot3/chapter2/SpringApp01.java
+++ src/main/java/io/df/henry/udemyspringboot3/chapter2/SpringApp01.java
@@ -1,5 +1,6 @@
 package io.df.henry.udemyspringboot3.chapter2;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringApp01 {
@@ -13,14 +14,18 @@ public class SpringApp01 {
     // 2-2. @Bean - name
 
     // 3. get bean of name.
-    System.out.println(context.getBean("name")); // Henry
-    System.out.println(context.getBean("age")); // 38
-    System.out.println(context.getBean("person")); // Person[name=George, age=38]
-    System.out.println(context.getBean("person2MethodCall")); // Person[name=George, age=38]
-    System.out.println(context.getBean("person3Parameters")); // Person[name=George, age=38]
+    System.out.println("name : "+context.getBean("name")); // Henry
+    System.out.println("age : "+context.getBean("age")); // 38
+    System.out.println("person : "+context.getBean("person")); // Person[name=George, age=38]
+    System.out.println("person2MethodCall : "+context.getBean("person2MethodCall")); // Person[name=George, age=38]
+    System.out.println("person3Parameters : "+context.getBean("person3Parameters")); // Person[name=George, age=38]
 //    System.out.println(context.getBean("address")); // Address[firstLine=Baker street, city=London]
 
-    System.out.println(context.getBean("address2")); // Bean 이름을 address2로 변경 함.
-//    System.out.println(context.getBean(Address.class)); // class 로도 찾을 수 있다.
+    System.out.println("address2 : "+context.getBean("address2")); // Bean 이름을 address2로 변경 함.
+    System.out.println("address by class type : "+context.getBean(Address.class)); // class 로도 찾을 수 있다.
+    System.out.println("person by class type : "+context.getBean(Person.class)); // class 로도 찾을 수 있다.
+
+    Arrays.stream(context.getBeanDefinitionNames())
+        .forEach(System.out::println);
   }
 }
